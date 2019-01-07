e82a0733f678600f110dae7f3ea732a194f9d723
AlexanderMac <amatsibarov@gmail.com>
2019-01-07 07:36:35 +0300

    integrate browser-sync

--- client/config/webpack/webpack.development.js
+++ client/config/webpack/webpack.development.js
@@ -1,10 +1,21 @@
 'use strict';
 
-const webpackMerge = require('webpack-merge');
-const commonConfig = require('./webpack.common.js');
+const webpackMerge      = require('webpack-merge');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const commonConfig      = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
   mode: 'development',
 
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new BrowserSyncPlugin({
+      proxy: 'localhost:3000',
+      port: 3001,
+      open: false,
+      notify: true,
+      reloadDelay: 500
+    })
+  ]
 });
