f1932e2e93995ae1d0547b6911a1e88c9793f8a3
AlexanderMac <amatsibarov@gmail.com>
2019-10-14 16:48:23 +0300

    fix server logger

--- server/src/_common/util/logger.js
+++ server/src/_common/util/logger.js
@@ -2,13 +2,33 @@ const _ = require('lodash');
 const winston = require('winston');
 const config = require('../../../config/environment');
 
-const transports = _.map(config.get('winston:transports'), (transportOpts, transportName) => {
-  switch (transportName) {
-    case 'console':
-      return new winston.transports.Console(transportOpts);
-    default:
-      throw new Error(`Invalid transport name: ${transportName}`);
-  }
+const { combine, colorize, simple } = winston.format;
+
+function customFormat() {
+  return combine(colorize(), simple());
+}
+
+let logger = winston.createLogger({
+  exitOnError: false,
+  transports: _.map(config.get('winston:transports'), (opts, transType) => {
+    switch (transType) {
+      case 'console':
+        return new winston.transports.Console({
+          level: opts.level,
+          format: customFormat()
+        });
+      default:
+        throw new Error('Unknown logger transport type: ' + transType);
+    }
+  })
 });
 
-module.exports = winston.createLogger({ transports });
+logger.error = message => {
+  if (message instanceof Error) {
+    logger.log({ level: 'error', message: message.stack });
+  } else {
+    logger.log({ level: 'error', message });
+  }
+};
+
+module.exports = logger;
